86dc1309258f60915689a069425962a32f05c658
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-08-20 16:59:11 -0400

    Add files via upload

--- Assign 10_3.sql
+++ Assign 10_3.sql
@@ -1,7 +1,7 @@
 INSERT INTO `bank`.`customer`
 (`c_id`,`cname`,`mno`,`address`,`city`,`DOB`)
 VALUES
-('C4','Ankur',838289,'Domlur','Bangalore','1999-09-11');
-select cname from customer having count(*)>2;
+('C6','John',838289,'Domlur','Bangalore','1999-09-11');
+select cname from `bank`.customer group by cname having (COUNT(*)>2) ;
 select * from customer group by cname;
 select * from customer order by cname;
