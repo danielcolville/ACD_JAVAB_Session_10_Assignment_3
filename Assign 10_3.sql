cf9a49ed9471a4c46defc98736b59ba271734bfd
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-08-21 09:31:57 -0400

    Update Assign 10_3.sql

--- Assign 10_3.sql
+++ Assign 10_3.sql
@@ -1,7 +1,11 @@
 INSERT INTO `bank`.`customer`
 (`c_id`,`cname`,`mno`,`address`,`city`,`DOB`)
 VALUES
-('C6','John',838289,'Domlur','Bangalore','1999-09-11');
+('C4','John',838289,'Domlur','Bangalore','1999-09-11');
+INSERT INTO `bank`.`customer`
+(`c_id`,`cname`,`mno`,`address`,`city`,`DOB`)
+VALUES
+('C5','John',838289,'Domlur','Bangalore','1999-09-11');
 select cname from `bank`.customer group by cname having (COUNT(*)>2) ;
 select * from customer group by cname;
 select * from customer order by cname;
